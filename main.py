fa74a23f27dae52bb290c249e61468ced4e30a45
xjyssg <jiayuexue30@gmail.com>
2020-02-19 09:42:34 +0200

    update

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 
-G = nx.read_edgelist("citation.edgelist")
-# nx.draw(G, node_size=80, node_color='red', with_labels=True)
-# plt.show()
-print(G.nodes())
+
+def network_structure(G):
+    summary = {}
+    summary["diameter"] = nx.diameter(G)
+    summary["radius"] = nx.radius(G)
+    summary["center"] = nx.center(G)
+    return summary
+
+def plot_network(G):
+    nx.draw(G, node_size=80, node_color='red', with_labels=True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    G = nx.read_edgelist("test.edgelist", create_using=nx.DiGraph)
+    print(network_structure(G))

31f6b5f53b270ac28c096e98c8dc50bdc731b09b
xjyssg <jiayuexue30@gmail.com>
2020-02-23 14:56:01 +0200

    update

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+def degree_distribution(DiG):
+    in_degrees = [DiG.in_degree(node) for node in DiG.nodes()]
+    plt.subplot(1, 2, 1)
+    plt.hist(in_degrees)
+    out_degrees = [DiG.out_degree(node) for node in DiG.nodes()]
+    plt.subplot(1, 2, 2)
+    plt.hist(out_degrees)
+    plt.savefig('./figures/degree_distribution.png')
+    plt.show()
+    return {"in": in_degrees, "out": out_degrees}
 
-def network_structure(G):
+def network_structure(DiG):
+    # G = DiG.to_undirected()
     summary = {}
-    summary["diameter"] = nx.diameter(G)
-    summary["radius"] = nx.radius(G)
-    summary["center"] = nx.center(G)
+    summary["diameter"] = nx.diameter(DiG)
+    summary["radius"] = nx.radius(DiG)
+    summary["center"] = nx.center(DiG)
+    clustering = nx.clustering(DiG)
+    for node in clustering.keys():
+        clustering[node] = round(clustering[node], 4)
+    summary["clustering"] = clustering
+    summary["num_strong"] = nx.number_strongly_connected_components(DiG)
+    summary["num_weak"] = nx.number_weakly_connected_components(DiG)
+
     return summary
 
 def plot_network(G):
@@ -17,3 +35,5 @@ def plot_network(G):
 if __name__ == "__main__":
     G = nx.read_edgelist("test.edgelist", create_using=nx.DiGraph)
     print(network_structure(G))
+    # plot_network(G)
+    degree_distribution(G)

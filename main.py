a9df9b62c63a84eec98acd45f9a7c1480c137d40
xjyssg <jiayuexue30@gmail.com>
2020-02-26 19:38:35 +0200

    update

--- main.py
+++ main.py
@@ -2,23 +2,30 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.special import comb
+import pandas as pd
+import numpy as np
 
-
-def degree_distribution(DiG):
-    in_degrees = [DiG.in_degree(node) for node in DiG.nodes()]
-    plt.subplot(1, 2, 1)
-    sns.distplot(in_degrees, rug=True)
-    out_degrees = [DiG.out_degree(node) for node in DiG.nodes()]
-    plt.xlabel('in-degree')
-    plt.ylabel('number')
-    plt.title('in-degree distribution')
-    plt.subplot(1, 2, 2)
-    sns.distplot(out_degrees, rug=True)
-    plt.xlabel('out-degree')
-    plt.ylabel('number')
-    plt.title('out-degree distribution')
-    plt.savefig('./figures/degree_distribution.png')
-    plt.show()
+def degree_distribution(DiG, plot=False):
+    in_degrees = {}
+    out_degrees = {}
+    for node in DiG.nodes():
+        in_degrees[node] = DiG.in_degree(node)
+        out_degrees[node] = DiG.out_degree(node)
+    if plot:
+        # plt.subplot(1, 2, 1)
+        sns.distplot(list(in_degrees.values()), rug=True)
+        plt.xlabel('in-degree')
+        plt.ylabel('number')
+        plt.title('in-degree distribution')
+        plt.savefig('./figures/in_degree_distribution.png')
+        plt.show()
+        # plt.subplot(1, 2, 2)
+        sns.distplot(list(out_degrees.values()), rug=True)
+        plt.xlabel('out-degree')
+        plt.ylabel('number')
+        plt.title('out-degree distribution')
+        plt.savefig('./figures/out_degree_distribution.png')
+        plt.show()
     return {"in": in_degrees, "out": out_degrees}
 
 def triadic_closure(G):
@@ -60,32 +67,93 @@ def network_structure(DiG):
     clustering = nx.clustering(DiG)
     for node in clustering.keys():
         clustering[node] = round(clustering[node], 4)
-    summary["clustering"] = clustering
+    # summary["clustering"] = clustering
     summary["num_strong"] = nx.number_strongly_connected_components(DiG)
     summary["num_weak"] = nx.number_weakly_connected_components(DiG)
-    if summary["num_strong"] == 1:
-        summary["diameter"] = nx.diameter(DiG)
-        summary["radius"] = nx.radius(DiG)
-        summary["center"] = nx.center(DiG)
-    
+    # if summary["num_weak"] == 1:
+    #     print("!@@@!")
+    #     summary["diameter"] = nx.diameter(G)
+    #     summary["radius"] = nx.radius(G)
+    #     summary["center"] = nx.center(G)
     return summary
 
 def spectral_network(DiG):
     nx.draw_spectral(DiG, node_size=200, with_labels=True)
     plt.show()
 
+def influenced_paper(DiG, plot=False):
+    ReDiG = DiG.reverse()
+    influenced = {}
+    for node in ReDiG.nodes():
+        node_list = []
+        for n1 in ReDiG.neighbors(node):
+            node_list.append(n1)
+            for n2 in ReDiG.neighbors(n1):
+                node_list.append(n2)
+        influenced[node] = len(set(node_list))
+    if plot:
+        sns.distplot(list(influenced.values()), rug=True)
+        plt.xlabel("papers")
+        plt.ylabel("number")
+        plt.title("influenced papers")
+        plt.savefig('./figures/influenced_papers.png')
+        plt.show()
+    return influenced
+
+def top_papers(records, num):
+    sorted_records = sorted(records.items(), key=lambda r:r[1], reverse=True)
+    tops = sorted_records[:num]
+    rank = 1
+    result = {}
+    for paper, citation in tops:
+        result[paper] = (rank, citation)
+        rank += 1
+    return result
+
+def citation_comparison(DiG, plot=False):
+    degrees = degree_distribution(DiG, plot)
+    num = 20
+    top_deg = top_papers(degrees["in"], num)
+    influenced = influenced_paper(DiG, plot)
+    top_inf = top_papers(influenced, num)
+    papers = set(top_deg.keys()).union(set(top_inf.keys()))
+    df = pd.DataFrame(columns=["deg", "inf"], index=papers)
+    for idx in df.index:
+        df["deg"][idx] = 1 / top_deg.get(idx, [-20])[0]
+        df["inf"][idx] = 1 / top_inf.get(idx, [-20])[0]
+    if plot:
+        df.plot(kind='bar')
+        plt.xlabel("papers")
+        plt.ylabel("influence")
+        plt.title("popular papers")
+        plt.savefig('./figures/citation_comparison.png')
+        plt.show()
 
 RUNPASSED = 0
 if __name__ == "__main__":
     # DiG = nx.read_edgelist("test.edgelist", create_using=nx.DiGraph)
     DiG = nx.read_edgelist("citation.edgelist", create_using=nx.DiGraph)
+    # print(network_structure(DiG))
     G = DiG.to_undirected()
-    subgraph = network_component(DiG)
-    summary = network_structure(subgraph)
-    print(summary["num_weak"])
-    print(summary["num_strong"])
-    # print(summary["num_node"])
+    # subgraph = network_component(DiG)
+    # summary = network_structure(subgraph)
+    # degrees = degree_distribution(DiG, plot=True)
+    # indeg = [degree for degree in list(degrees['in'].values()) if degree < 400]
+    # sns.distplot(indeg, rug=True)
+    # plt.show()
+    # print(summary)
+    # citation_comparison(DiG,True)
+    lambd1 = 0.7
+    lambd2 = 0.4
+    x = np.arange(0,15,0.1)
+    y1 = lambd1*np.exp(-lambd1*x)
+    y2 = lambd2*np.exp(-lambd2*x)
+    l1, = plt.plot(x,y1)
+    plt.title("exponential distribution")
+    plt.legend(handles=[l1,], labels=['lambda=0.7',],  loc='best')
+    plt.show()
+
     if RUNPASSED:
+        influenced_paper(DiG)
         triadic_closure(G)
-        degree_distribution(DiG)
         spectral_network(DiG)
